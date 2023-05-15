2ab973a440428d49a39f21635354a32d2300c07c
unknown <mukhammadjonoff@gmail.com>
2023-05-15 13:07:56 -0700

    abc

--- views/layouts/main-frontend.php
+++ views/layouts/main-frontend.php
@@ -1,7 +1,12 @@
 <?php
+/* @var $this \yii\web\View */
+/* @var $content string */
+
 $this->title = ' ';
+$this->registerCssFile('/frontend/assets/dist/css/bootstrap.min.css');
+$this->registerCssFile('/frontend/assets/dist/css/cover.css');
+$this->registerJsFile('/frontend/assets/dist/js/bootstrap.bundle.min.js');
+
 ?>
 <?= $this->render('content', ['content' => $content]) ?>
 <?= $this->render('footer-origin') ?>
-
-
