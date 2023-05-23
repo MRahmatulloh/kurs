276ebe3237d4f33706c48b24e733dec48151745d
Rahmatulloh <r.muxammadjonov@uzinfocom.uz>
2023-05-23 20:51:27 +0500

    somesd

--- views/book/index.php
+++ views/book/index.php
@@ -51,26 +51,27 @@ $this->params['breadcrumbs'][] = $this->title;
     }
 </style>
 <div class="book-index container">
-    <div class="col-10 m-auto">
+    <div class="col-12 m-auto">
 
         <div class="container">
             <div class="row">
-                <?php
-                /**
-                 * @var Book $book
-                 */
-                foreach ($dataProvider->getModels() as $book): ?>
-                    <div class="col-4">
-                        <div class="card">
-                            <div class="card-img d-flex justify-content-center">
-                                <img class="card-img-top"
-                                     src="<?= Yii::getAlias('@web') . '/img/books/' . ($book->photo ?? 'no-photo.png') ?>"
-                                     alt="">
-                            </div>
-                            <div class="card-body bg3 fs-6 text-white text-start">
-                                <h5><?= $book->name ?></h5>
-                                <p><?= $book->description ?></p>
-                                <span class="d-flex justify-content-between align-items-center">
+                <div class="col-10 m-auto">
+                    <?php
+                    /**
+                     * @var Book $book
+                     */
+                    foreach ($dataProvider->getModels() as $book): ?>
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-img d-flex justify-content-center">
+                                    <img class="card-img-top"
+                                         src="<?= Yii::getAlias('@web') . '/img/books/' . ($book->photo ?? 'no-photo.png') ?>"
+                                         alt="">
+                                </div>
+                                <div class="card-body bg3 fs-6 text-white text-start">
+                                    <h5><?= $book->name ?></h5>
+                                    <p><?= $book->description ?></p>
+                                    <span class="d-flex justify-content-between align-items-center">
                                     <?php if (Yii::$app->user->identity->isRoleUser('admin')): ?>
                                         <?php if ($book->price): ?>
                                             <span class="fs-6"><?= pul2($book->price, 2) ?> so'm</span>
@@ -106,18 +107,22 @@ $this->params['breadcrumbs'][] = $this->title;
                                         <?php endif; ?>
                                     <?php endif ?>
                                 </span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                <?php endforeach; ?>
+                    <?php endforeach; ?>
+
+                    <?php if (Yii::$app->user->can('admin')): ?>
+                        <div class="col-4 bg-light pb-3">
+                            <div class="card h-100 d-flex flex-row justify-content-center align-items-center py-5">
+                                <?= Html::a('<i class="fa fa-7x fa-fw fa-plus text-white"></i>', ['create'], ['class' => 'btn btn-light border-0 text-center img-circle elevation-3 bg-gray', 'title' => 'Qo\'shish']) ?>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+
+                </div>
 
                 <?php if (Yii::$app->user->can('admin')): ?>
-                    <div class="col-4 bg-light pb-3">
-                        <div class="card h-100 d-flex flex-row justify-content-center align-items-center py-5">
-                            <?= Html::a('<i class="fa fa-7x fa-fw fa-plus text-white"></i>', ['create'], ['class' => 'btn btn-light border-0 text-center img-circle elevation-3 bg-gray', 'title' => 'Qo\'shish']) ?>
-                        </div>
-                    </div>
-                <?php else: ?>
                     <div class="col-12 text-center section-buy">
                         <div class="d-flex justify-content-center my-5">
                             <form method="post" action="<?= \yii\helpers\Url::to(['order/buy']) ?>">
